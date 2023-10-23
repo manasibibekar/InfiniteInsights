55922b62e878801a537635449c11a2f6a590a705
Manasi Bibekar <manasibibekar@gmail.com>
2023-10-23 21:23:54 +0530

    changed password from stringfield to passwordfield

--- forms.py
+++ forms.py
@@ -15,7 +15,7 @@ class CreatePostForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Sign me up!")
 
